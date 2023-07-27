a13b073837f65cc06fcbb98baee099e287e4965b
taohuayu <2251221623@qq.com>
2023-07-27 22:50:32 +0800

    render函数、ref属性

---
+++ vue-脚手架/vue-test/01-src-分析脚手架/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+   <student></student>
+   <hr>
+   <school></school>
+  </div>
+</template>
+
+<script>
+import School from './components/School.vue'
+import Student from './components/student.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Student,
+    School
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
