b064b8856245a076715115d884c308c2608ff675
taohuayu <2251221623@qq.com>
2023-07-28 10:36:07 +0800

    props配置

---
+++ vue-脚手架/vue-test/03-src-props配置/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <student  name="科文" address="徐州" :age="22"></student>
+  </div>
+</template>
+
+<script>
+import Student from './components/Student.vue'
+
+export default {
+    name:'App',
+  components: { Student },
+  
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-脚手架/vue-test/03-src-props配置/components/Student.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <h1>学校名称：{{name}}</h1>
+    <h1>学校地址：{{address}}</h1>
+    <h1>学校历时：{{age}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    // eslint-disable-next-line vue/multi-word-component-names
+    name:'School',
+    data(){
+        return{
+           
+        }
+    },
+    //简单声明接收
+    //props:["name","address","age"]
+
+    //申明的同时对数据类型进行限制
+    // props:{
+    //   name:String,
+    //   address:String,
+    //   age:Number
+    // }
+
+    //申明的同时对数据进行类型限制、默认值、必要性限制
+    props:{
+      name:{
+        type:String,
+        require:true
+      },
+      address:{
+        type:String,
+        require:true
+      },
+      age:{
+        type:Number,
+        default:100
+      }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
