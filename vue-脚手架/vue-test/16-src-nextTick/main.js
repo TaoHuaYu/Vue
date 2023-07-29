55ee5acfcd1a0ce880beffd0cc37ed71ae75c29a
taohuayu <2251221623@qq.com>
2023-07-29 22:00:05 +0800

    nextTick

---
+++ vue-脚手架/vue-test/16-src-nextTick/main.js
@@ -0,0 +1,17 @@
+//引入vue
+import Vue from 'vue'
+// 引入app
+import App from './App'
+import { mixin1 } from './mixin'
+// 关闭vue生产提示
+Vue.config.productionTip=false
+Vue.mixin(mixin1)
+
+// 创建vm实例
+new Vue({
+    el:'#app',
+    render: h=>h(App),
+    beforeCreate(){
+        Vue.prototype.$bus=this;
+    }
+})
