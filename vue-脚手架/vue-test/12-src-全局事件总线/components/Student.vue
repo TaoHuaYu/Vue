d14fcd19bb8c1e233629c53d54a3773d75c1fe0a
taohuayu <2251221623@qq.com>
2023-07-29 17:00:54 +0800

    全局事件总线

---
+++ vue-脚手架/vue-test/12-src-全局事件总线/components/Student.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="demo">
+      <h1>学生名称：{{name}}</h1>
+      <h1>学生地址：{{address}}</h1>
+      <h1>学生年龄：{{age}}</h1>
+      <button type="button" @click="sendName()">发送学生的姓名给school组件</button>
+    </div>
+  </template>
+  
+  <script>
+  
+  export default {
+      // eslint-disable-next-line vue/multi-word-component-names
+      name:'Student',
+      data(){
+          return{
+            name:'ttt',
+            address:'yangzhou',
+            age:22
+             
+          }
+      },  
+      methods:{
+        sendName(){
+          this.$bus.$emit('hello',this.name);
+        }
+      }
+  }
+  </script>
+  
+  <style scoped>
+  .demo{
+    background-color: skyblue;
+  }
+  
+  </style>
---
+++ vue-脚手架/vue-test/src/components/School.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="demo">
+    <h1>学校名称：{{ name }}</h1>
+    <h1>学校地址：{{ address }}</h1>
+    <h1>学校历时：{{ age }}</h1>
+    <h1>学生的姓名：{{studentName}}</h1>
+  </div>
+</template>
+  
+  <script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "School",
+  props: ["getSchoolName"],
+  data() {
+    return {
+      name: "kewen",
+      address: "xuzhou",
+      age: 22,
+      studentName:''
+    };
+  },
+  mounted(){
+    this.$bus.$on('hello',value=>this.studentName=value);
+  },
+  beforeDestroy(){
+    this.$bus.$off('hello');
+  }
+};
+</script>
+  
+  <style scoped>
+.demo {
+  background-color: greenyellow;
+}
+</style>
