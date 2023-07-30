5327ac1aca8ef85503e6cde36fa0e30e4ead9b7f
taohuayu <2251221623@qq.com>
2023-07-30 09:50:11 +0800

    配置代理服务器

---
+++ vue-脚手架/vue-test/17-src-动画与过度/components/Test.vue
@@ -0,0 +1,40 @@
+<template lang="html">
+    <div >
+        <transition appear name="hello">
+            <h1 class="show" v-show="isShow" >hello world!!!!</h1>
+        </transition>
+        <button type="button" @click="isShow=!isShow">显示/隐藏</button>
+    </div>
+</template>
+<script>
+export default {
+    // eslint-disable-next-line vue/multi-word-component-names
+    name:'Test',
+    data() {
+        return {
+            isShow:true
+        }
+    },
+    
+}
+</script>
+<style >
+.show{
+    background-color: skyblue;
+}
+.hello-enter-active{
+    animation: tao  1s ;
+}
+.hello-leave-active{
+    animation: tao 1s reverse;
+
+}
+@keyframes tao {
+    from{
+        transform: translateX(-100%);
+    }
+    to{
+        transform: translateX(0px);
+    }
+}
+</style>
