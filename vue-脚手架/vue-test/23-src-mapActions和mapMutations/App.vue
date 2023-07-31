851903bc52ed4a95bbc723bcc6d59784e396d9d8
taohuayu <2251221623@qq.com>
2023-07-31 11:12:49 +0800

    mapActions与mapMutations

---
+++ vue-脚手架/vue-test/23-src-mapActions和mapMutations/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div >
+    <count></count>
+  </div>
+</template>
+
+<script>
+import Count from './components/Count.vue';
+export default {
+  components: { Count },
+  name: "App",
+};
+</script>
+
+<style>
+
+</style>
