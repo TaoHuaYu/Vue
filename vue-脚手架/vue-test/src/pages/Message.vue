31a5d24cb678bef76ca0659ab1fd0bcfd8f67419
taohuayu <2251221623@qq.com>
2023-08-02 22:19:55 +0800

    2023/8/2路由

--- vue-脚手架/vue-test/src/pages/Message.vue
+++ vue-脚手架/vue-test/src/pages/Message.vue
@@ -1,9 +1,25 @@
 <template>
   <div>
     <ul>
-        <li v-for="(item,index) in list" :key="index">{{ item }}</li>
+        <li v-for="item in list" :key="item.id">
+          <!-- 跳转路由并携带query参数，to的字符串写法 -->
+          <!-- <router-link :to="`/home/message/detail?id=${item.id}&title=${item.title}`"> {{ item.title }}</router-link>  -->
+          
+          <!-- 跳转路由并携带query参数，to的对象写法 -->
+          <router-link :to="{
+            name:'Detail',
+            params:{
+              id:item.id,
+              title:item.title
+            }
+          }"> 
+          {{ item.title }}
+        </router-link> 
+        <button type="button" @click="pushShow(item)">pushShow</button>
+        <button type="button" @click="replaceShow(item)">replaceShow</button>
+        </li>
     </ul>
-
+    <router-view></router-view>
   </div>
 </template>
 
@@ -13,11 +29,33 @@ export default {
     name:'Message',
     data() {
         return {
-            list:[1,2,3,4]
-            
+            list:[
+              {id:'001',title:"111111111"},
+              {id:'002',title:"222222222"},
+              {id:'003',title:"333333333"},
+            ]
         }
     },
-
+    methods:{
+      pushShow(item){
+        this.$router.push({
+          name:'Detail',
+          params:{
+            id:item.id,
+            title:item.title
+          }
+        })
+      },
+      replaceShow(item){
+        this.$router.replace({
+          name:'Detail',
+          params:{
+            id:item.id,
+            title:item.title
+          }
+        })
+      }
+    }
 }
 </script>
 
--- vue-脚手架/vue-test/src/router/index.js
+++ vue-脚手架/vue-test/src/router/index.js
@@ -5,7 +5,8 @@ import VueRouter from "vue-router";
 import About from '@/pages/About';
 import Home from '@/pages/Home';
 import Message from '@/pages/Message';
-import New from '@/pages/New'
+import New from '@/pages/New';
+import Detail from '@/pages/Detail'
 //创建router实例对象，去管理一组一组的路由规则
 export default new VueRouter({
     routes:[
@@ -22,7 +23,17 @@ export default new VueRouter({
                 {
                     path:'message',
                     name:'Message',
-                    component:Message
+                    component:Message,
+                    children:[
+                        {
+                            path:'detail/:id/:title',
+                            name:'Detail',
+                            component:Detail,
+                            props($route){
+                                return {id:$route.params.id,title:$route.params.title}
+                            }
+                        }
+                    ]
                 },
                 {
                     path:'new',
