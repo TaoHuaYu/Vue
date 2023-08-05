b8d8ae53c4ea128fd08b7e99c8024723305d52c7
taohuayu <2251221623@qq.com>
2023-08-05 22:59:41 +0800

    watch函数

--- vue3/vue3_test/05-src-setup中的两个注意点/App.vue
+++ vue3/vue3_test/05-src-setup中的两个注意点/App.vue
@@ -1,30 +1,26 @@
 <template>
- <demo msg="hello world!!!" @hello="showInfo">
-  <template v-slot:tao>
-    <div>123123</div>
-  </template>
-   
- </demo>
+  <demo msg="hello world!!!" @hello="showInfo">
+    <template v-slot:tao>
+      <div>123123</div>
+    </template>
+  </demo>
 </template>
 
 <script>
-import Demo from './components/Demo.vue'
+import Demo from "./components/Demo.vue";
 
 export default {
   components: { Demo },
-  name: 'App',
- setup(){
-  function showInfo(value){
-    alert("value:"+value)
-  }
-  return{
-    showInfo
-  }
- }
-
-}
+  name: "App",
+  setup() {
+    function showInfo(value) {
+      alert("value:" + value);
+    }
+    return {
+      showInfo,
+    };
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
---
+++ vue3/vue3_test/07-src-watch函数/App.vue
@@ -0,0 +1,25 @@
+<template>
+ <demo ></demo>
+</template>
+
+<script>
+import Demo from './components/Demo.vue'
+
+export default {
+  components: { Demo },
+  name: 'App',
+ setup(){
+  function showInfo(value){
+    alert("value:"+value)
+  }
+  return{
+    showInfo
+  }
+ }
+
+}
+</script>
+
+<style>
+
+</style>
