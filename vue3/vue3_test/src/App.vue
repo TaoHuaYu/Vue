1be167806bd545a36f549d3242a073d2aeac0793
taohuayu <2251221623@qq.com>
2023-08-06 15:44:56 +0800

    shallReactive/shallRef

--- vue3/vue3_test/src/App.vue
+++ vue3/vue3_test/src/App.vue
@@ -1,16 +1,14 @@
 <template>
  <demo v-if="isShow"></demo><br/>
  <button type="button" @click="Show()">展示/隐藏</button>
- <test></test>
 </template>
 
 <script>
 import { ref } from 'vue'
 import Demo from './components/Demo.vue'
-import Test from './components/Test.vue'
 
 export default {
-  components: { Demo, Test },
+  components: { Demo},
   name: 'App',
  setup(){
   let isShow=ref(true) 
--- vue3/vue3_test/src/components/Demo.vue
+++ vue3/vue3_test/src/components/Demo.vue
@@ -1,24 +1,33 @@
  <template>
-  <h1>{{ sum }}</h1>
-  <button type="button" @click="sum++">++</button>
-  <h1>当前点击的坐标为{{point.x}},{{ point.y }}</h1>
+  <h4>{{person}}</h4>
+	<h2>姓名：{{name}}</h2>
+	<h2>年龄：{{age}}</h2>
+	<h2>薪资：{{job.j1.salary}}K</h2>
+	<button @click="name+='~'">修改姓名</button>
+	<button @click="age++">增长年龄</button>
+	<button @click="job.j1.salary++">涨薪</button>
 </template>
 
   <script>
-import { ref } from "vue";
-import usePoint from '@/hooks/usePoint'
+import { reactive, toRefs } from "vue";
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
   name: "Demo",
   setup() {
-    const sum = ref(0);
-    const msg = ref("hello");
-    let point=usePoint();
+    let person = reactive({
+				name:'张三',
+				age:18,
+				job:{
+					j1:{
+						salary:20
+					}
+				}
+			})
+   
 
     return {
-      sum,
-      msg,
-      point,
+      person,
+      ...toRefs(person)
     };
   },
 };
